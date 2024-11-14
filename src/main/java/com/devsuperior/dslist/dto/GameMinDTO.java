ed7a5cf7875d71e9de2e2524eb43f7747693405e
joser <joserbtgt@gmail.com>
2024-11-14 16:06:46 -0300

    Homolog Ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -30,7 +30,7 @@ public class GameMinDTO {
     public GameMinDTO(GameMinProjection entity) {
         id = entity.getId();
         title = entity.getTitle();
-        year = entity.getYear();
+        year = entity.getGameYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
     }
